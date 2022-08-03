66b6f8bab23fd734a13566418498a60116d32330
RuslanSkl <rsklyarenko@gmail.com>
2022-08-03 13:25:10 +0300

    Change task 5 after yesterday lesson

--- 02/js/sort.js
+++ 02/js/sort.js
@@ -1,3 +1,7 @@
-let userDigit = prompt('Введіть будь-ласка будь-яке пятизначне число!');
-let newDigit = userDigit .split('');
-alert(newDigit);
+const varNumber1 = parseInt (prompt("Введите пожалуйста пятизначное число"));
+const r1 = varNumber1 % 10;
+const r2 = ((varNumber1 - r1) / 10) % 10;
+const r3 = ((((varNumber1 - r1) / 10) - r2) / 10) % 10;
+const r4 = (((((varNumber1 - r1) / 10) - r2) /10 - r3) /10) % 10;
+const r5 = (((((((varNumber1 - r1) / 10) - r2) /10 - r3) / 10 - r4) / 10)); 
+alert (r5 + ' ' + r4 + ' ' + r3 + ' ' + r2 + ' ' + r1);
