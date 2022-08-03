425aee0e8b026ca9c075c192fdee84f6fbd04b65
RuslanSkl <rsklyarenko@gmail.com>
2022-08-03 16:19:37 +0300

    all commit

--- 02/js/sort.js
+++ 02/js/sort.js
@@ -5,4 +5,3 @@ const r3 = ((((varNumber1 - r1) / 10) - r2) / 10) % 10;
 const r4 = (((((varNumber1 - r1) / 10) - r2) /10 - r3) /10) % 10;
 const r5 = (((((((varNumber1 - r1) / 10) - r2) /10 - r3) / 10 - r4) / 10)); 
 alert (r5 + ' ' + r4 + ' ' + r3 + ' ' + r2 + ' ' + r1);
-
