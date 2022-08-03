5f1ca05a24bc44106173cbc6fc938c61e43b0edd
RuslanSkl <rsklyarenko@gmail.com>
2022-08-03 16:15:34 +0300

    Made first task about same digits after 3 lesson

---
+++ 03/task_1/same_digits.js
@@ -0,0 +1,10 @@
+const userNumber = parseInt(prompt('Введите пожалуйста трехзначное число!'));
+const digit1 = userNumber % 10;
+const digit2 = ((userNumber - digit1) / 10) % 10;
+const digit3 = ((((userNumber - digit1) / 10) - digit2) / 10) % 10;
+if (digit1 == digit2 && digit2 == digit3 && digit1 == digit3) {
+    console.log("Все цифры одинаковые");
+} 
+else { (digit1 == digit2 || digit2 == digit3 || digit1 == digit3) 
+    console.log("Есть одинаковые цифры"); 
+} 
