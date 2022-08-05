209ff94d02cb456557075c3df8b3aefc17543676
RuslanSkl <rsklyarenko@gmail.com>
2022-08-05 15:48:30 +0300

    Change concatenation.js to more right

--- 02/js/concatenation.js
+++ 02/js/concatenation.js
@@ -1,5 +1,5 @@
-const firstStr = String (prompt('Введить будь-ласка першу строку'));
-const secondStr = String (prompt('Введить будь-ласка другу строку'));
-const thirdStr = String (prompt('Введить будь-ласка третю строку'));
-alert(firstStr + secondStr + thirdStr);
+const firstStr = prompt('Введить будь-ласка першу строку');
+const secondStr = prompt('Введить будь-ласка другу строку');
+const thirdStr = prompt('Введить будь-ласка третю строку');
+alert(`${firstStr} ${secondStr} ${thirdStr}`);
 
