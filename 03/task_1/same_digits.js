a1776539a7ef9e97bae1eddab9f2edb728a30cb7
RuslanSkl <rsklyarenko@gmail.com>
2022-08-05 16:26:25 +0300

    Add else to same_digits for user friendly

--- 03/task_1/same_digits.js
+++ 03/task_1/same_digits.js
@@ -5,6 +5,10 @@ const digit3 = ((((userNumber - digit1) / 10) - digit2) / 10) % 10;
 if (digit1 == digit2 && digit2 == digit3 && digit1 == digit3) {
     console.log("Все цифры одинаковые");
 } 
-else { (digit1 == digit2 || digit2 == digit3 || digit1 == digit3) 
+else if  (digit1 == digit2 || digit2 == digit3 || digit1 == digit3) { 
     console.log("Есть одинаковые цифры"); 
-} 
+}
+
+else {
+    console.log("Одинаковых цифр нет");
+}
