217837bb87c83fd31a94a72d134f5adcf0a1acf0
RuslanSkl <rsklyarenko@gmail.com>
2022-08-12 03:48:18 +0300

    Made almost in task 2 folder 05

--- 05/task_2/array_2.js
+++ 05/task_2/array_2.js
@@ -1,7 +1,66 @@
-const arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, 235, 4, 47, 5];
+let arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, 235, 4, 47, 5];
 
 let sumPosValues = 0;
-
-arr.forEach(function(item){
-    // if (item>=0) +=
+arr.forEach(function(item) {
+    if (item >= 0) {
+       sumPosValues += item; 
+    }
 });
+console.log ('Сумма положительных значений =' + ' ' + sumPosValues);
+
+let amountPosElem = arr.filter(function(item) {
+    return item >= 0;
+}); 
+console.log ('Количество положительных элементов =' + ' ' + amountPosElem.length);
+
+const minElement = Math.min(...arr);
+console.log ('Минимальный элемент массива =' + ' ' + minElement);
+console.log ('Индекс минимального элемента массива =' + ' ' + arr.indexOf(minElement));
+
+const maxElement = Math.max(...arr);
+console.log ('Максимальный элемент массива =' + ' ' + maxElement);
+console.log ('Индекс максимального элемента массива =' + ' ' + arr.indexOf(maxElement));
+
+let amountNegElem = arr.filter(function(item) {
+    return item < 0;
+}); 
+console.log ('Количество отрицательных элементов =' + ' ' + amountNegElem.length);
+
+let amountOddPosElem = arr.filter(function(item) {
+    return (item >= 0) && (item % 2 !== 0);
+}); 
+console.log ('Количество нечетных положительных элементов =' + ' ' + amountOddPosElem.length);
+
+let sumEvenPosElem = 0 
+arr.forEach(function(item) {
+    if ((item >= 0) && (item % 2 === 0)) {
+        sumEvenPosElem += item;
+    }
+}); 
+console.log ('Сумма четных положительных элементов =' + ' ' + sumEvenPosElem);
+
+
+let newArr = arr.filter(function(item) {
+    return (item >= 0);
+});
+let multipPosElem = newArr.reduce(function(prev, value) {
+    return prev * value;     
+}, 1);
+console.log ('Произведение положительных элементов =' + ' ' + multipPosElem);
+
+
+
+
+// let max = arr[0];
+// let maxi = 0;
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         max = arr[i];      
+//         arr[maxi] = 0;
+//         maxi = i;
+//     } else {
+//     arr[i] = 0;
+//     }
+// }
+
+
