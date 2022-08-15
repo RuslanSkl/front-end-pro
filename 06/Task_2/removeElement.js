1811bb1a340547264a790daa96c6235d388a0c38
RuslanSkl <rsklyarenko@gmail.com>
2022-08-15 12:35:07 +0300

    Made task 2 in folder 06

---
+++ 06/Task_2/removeElement.js
@@ -0,0 +1,12 @@
+// const arr = ['one', 'two', 'three'];
+
+function removeElement(index, item) {
+    index = arr.indexOf(item);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+}
+
+// removeElement(1,'two');
+// console.log (arr)
+
