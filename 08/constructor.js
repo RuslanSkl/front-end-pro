d4ef1e53c1443daa1c2afb0d95d44fcbdc07f666
RuslanSkl <rsklyarenko@gmail.com>
2022-08-25 22:57:03 +0300

    Fix constructor in folder 08

--- 08/constructor.js
+++ 08/constructor.js
@@ -1,49 +1,48 @@
-function User (name, age) {
-    this.name = name,
-    this.age = age,
-    this.info = function() {
-        alert('Hi ' + this.name + ' \nYour age is: ' + this.age);
-    }
+function User(name, age) {
+  this.name = name,
+  this.age = age,
+  this.info = function () {
+      alert ("Hi " + this.name + " \nYour age is: " + this.age);
+    };
 };
 
 let newUser;
 do {
-    newUser = prompt ("Enter owner name:");
-} while (newUser === null || newUser === '');
+  newUser = prompt("Enter owner name:");
+} while (!newUser)
 let userAge;
 do {
-    userAge = +prompt ("Enter owner age:");
-} while (userAge < 18 || userAge === null || userAge === '' || isNaN(userAge));
+  userAge = +prompt("Enter owner age:");
+} while (userAge < 18 || !userAge || isNaN(userAge));
 
-const owner = new User (newUser, userAge);
+const owner = new User(newUser, userAge);
 owner.info();
 
-function Automobile (model, year, mileage) {
-    this.model = model,
-    this.year = year,
-    this.mileage = mileage,
-    this.carInfo = function() {
-        alert ('Car model is: ' + this.model + ' \nYear of production is: ' + this.year + ' \nMileage is: ' + this.mileage)
+function Automobile(model, year, mileage) {
+  this.model = model,
+  this.year = year,
+  this.mileage = mileage,
+  this.carInfo = function () {
+      alert ("Car model is: " + this.model + " \nYear of production is: " + this.year + " \nMileage is: " + this.mileage);
     };
-    this.updateOwner = function () {
-        alert (`New owner of the car is: ${owner.name}`)
-    };
+  this.updateOwner = function () {
+    alert(`New owner of the car is: ${owner.name}`);
+  };
 };
-
-let newCar 
+let newCar;
 do {
-    newCar = prompt ("Enter a car model:");
-} while (newCar === null || newCar === ''); 
+  newCar = prompt("Enter a car model:");
+} while (!newCar);
 let newYearOfProduction;
 do {
-    newYearOfProduction = +prompt ("Enter a year of production:");
-} while (newYearOfProduction === null || newYearOfProduction === '' || isNaN(newYearOfProduction) || newYearOfProduction < 1900);
+  newYearOfProduction = +prompt("Enter a year of production:");
+} while (!newYearOfProduction || isNaN(newYearOfProduction) || newYearOfProduction < 1900);
 let newMileage;
 do {
-    newMileage = +prompt ("Enter a car mileage:");
-} while (newMileage === null || newMileage === '' || isNaN(newMileage) || newMileage < 0);
+  newMileage = +prompt("Enter a car mileage:");
+} while (!newMileage || isNaN(newMileage) || newMileage < 0);
 
-const ownerCar = new Automobile (newCar, newYearOfProduction, newMileage);
+const ownerCar = new Automobile(newCar, newYearOfProduction, newMileage);
 ownerCar.carInfo();
 
 ownerCar["newOwner"] = owner;
