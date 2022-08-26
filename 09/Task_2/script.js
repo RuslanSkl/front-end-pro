67c632de712ecdd76d88cd51125098341628a587
RuslanSkl <rsklyarenko@gmail.com>
2022-08-26 15:29:54 +0300

    Made task 2 in folder 09

---
+++ 09/Task_2/script.js
@@ -0,0 +1,10 @@
+const firstBtn = document.querySelector(".btn-get");
+const secondBtn = document.querySelector(".btn-push");
+let userUrl;
+firstBtn.addEventListener("click", function () {
+  userUrl = prompt ("Please enter a URL:");
+});
+secondBtn.addEventListener("click", function () {
+  document.location.href = userUrl;
+});
+
