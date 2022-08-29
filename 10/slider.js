291f786734779317d9f7f527a5d424d9a7b98b3d
RuslanSkl <rsklyarenko@gmail.com>
2022-08-29 18:43:07 +0300

    Made task 'Slider' in folder 10

---
+++ 10/slider.js
@@ -0,0 +1,28 @@
+"use strict";
+const btnPrev = document.querySelector("#prev");
+const btnNext = document.querySelector("#next");
+const content = document.querySelector(".content");
+const img = content.querySelector("img");
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"];
+img.src = "img/" + images[0];
+let i = 0;
+btnNext.addEventListener("click", function () {
+  ++i;
+  if (i > images.length - 1) {
+    i = 0;
+  }
+  img.src = "img/" + images[i];
+  console.log("User click `Next` button");
+});
+btnPrev.addEventListener("click", function () {
+  --i;
+  if (i < 0) {
+    i = images.length - 1;
+  }
+  img.src = "img/" + images[i];
+  console.log("User click `Prev` button");
+});
+
+const website = document.querySelector("body").addEventListener("click", function () {
+    console.log('User click on website');
+    });
