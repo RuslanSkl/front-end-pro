6007a370773d0db14c87f0a7ae7c7f6c68402759
RuslanSkl <rsklyarenko@gmail.com>
2022-09-03 23:10:43 +0300

    Made task in folder 11

--- 11/toDoList.js
+++ 11/toDoList.js
@@ -1,31 +1,72 @@
 "use strict";
-const lineThrough = "checkbtn";
-document.querySelector("#addTask").onclick = function () {
-  if (document.querySelector("#newtask input").value.length == 0) {
-    alert("You didn't fill in the field!");
-  } else {
-    document.querySelector("#newTasks").innerHTML += `
-          <div class="task">
-              <span id="taskname">
-                  ${document.querySelector("#newtask input").value}
-              </span>
-              <button class="delete">
-                  Delete
-              </button>
-              <input type=checkbox class="check"></input>
-          </div>
-      `;
-    let currentTasks = document.querySelectorAll(".delete");
-    for (let i = 0; i < currentTasks.length; i++) {
-      currentTasks[i].onclick = function () {
-        this.parentNode.remove();
-      };
+const todoForm = document.forms.todo;
+const taskList = document.querySelector(".taskList");
+const errorClass = "error";
 
-      let check = document.querySelectorAll(".check");
-      check.addEventListener("change", function () {
-          this.parentNode.classList.toggle(lineThrough);
-        });
-      
-    }
-  }
+function getErrorElement(message) {
+  const errorMsg = document.createElement("p");
+  errorMsg.classList.add("errorText");
+  errorMsg.textContent = message;
+  return errorMsg;
 }
+
+function addErrorMessage(inputElemnt, message) {
+  inputElemnt.classList.add(errorClass);
+  const error = getErrorElement(message);
+  inputElemnt.closest("#fieldWrapper").before(error);
+}
+
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (!todoForm.elements.task.value.trim().length) {
+    addErrorMessage(todoForm.elements.task, "Field mustn't be empty!");
+    return;
+  }
+
+  const taskWrapper = document.createElement("div");
+  taskWrapper.classList.add("taskWrapper");
+
+  const text = document.createElement("p");
+  text.setAttribute("class", "taskText");
+  text.textContent = todoForm.elements.task.value.trim();
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("value", "isDone");
+
+  const btnDelete = document.createElement("button");
+  btnDelete.setAttribute("class", "btnDelete");
+  btnDelete.innerText = "Delete task";
+
+  taskWrapper.append(checkbox);
+  taskWrapper.append(text);
+  taskWrapper.append(btnDelete);
+
+  taskList.append(taskWrapper);
+
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      this.closest(".taskWrapper").querySelector("p").classList.add("doneTask");
+    } else {
+      this.closest(".taskWrapper")
+        .querySelector("p")
+        .classList.remove("doneTask");
+    }
+  });
+
+  btnDelete.addEventListener("click", function () {
+    text.remove();
+    checkbox.remove();
+    btnDelete.remove();
+    taskWrapper.remove();
+  });
+});
+
+todoForm.querySelectorAll("input").forEach(function (input) {
+  input.addEventListener("input", function () {
+    this.classList.remove(errorClass);
+    this.closest("#todo")
+      .querySelectorAll(".errorText")
+      .forEach((error) => error.remove());
+  });
+});
