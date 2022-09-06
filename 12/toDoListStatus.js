958c5ce66814b8a16d5300285bb480ff3a264519
RuslanSkl <rsklyarenko@gmail.com>
2022-09-06 11:54:23 +0300

    Add task in folder 12 to git

---
+++ 12/toDoListStatus.js
@@ -0,0 +1,121 @@
+"use strict";
+const todoForm = document.forms.todo;
+const taskList = document.querySelector(".taskList");
+const errorClass = "error";
+
+const tasksArray = [];
+
+function getErrorElement(message) {
+  const errorMsg = document.createElement("p");
+  errorMsg.classList.add("errorText");
+  errorMsg.textContent = message;
+  return errorMsg;
+}
+
+function addErrorMessage(inputElemnt, message) {
+  inputElemnt.classList.add(errorClass);
+  const error = getErrorElement(message);
+  inputElemnt.closest("#fieldWrapper").before(error);
+}
+
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (!todoForm.elements.task.value.trim().length) {
+    addErrorMessage(todoForm.elements.task, "Field mustn't be empty!");
+    return;
+  }
+
+  const newTask = {
+    id: tasksArray.length === 0 ? 0 : tasksArray[tasksArray.length - 1].id + 1,
+    name: todoForm.elements.task.value,
+    isDone: false,
+  };
+
+  tasksArray.push(newTask);
+  console.log(tasksArray);
+
+  const taskWrapper = document.createElement("div");
+  taskWrapper.classList.add("taskWrapper");
+  taskWrapper.setAttribute("data-id", newTask.id);
+
+  const text = document.createElement("p");
+  text.setAttribute("class", "taskText");
+  text.textContent = todoForm.elements.task.value.trim();
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("value", "isDone");
+
+  const btnDelete = document.createElement("button");
+  btnDelete.setAttribute("class", "btnDelete");
+  btnDelete.innerText = "Delete task";
+
+  taskWrapper.append(checkbox);
+  taskWrapper.append(text);
+  taskWrapper.append(btnDelete);
+
+  taskList.append(taskWrapper);
+
+  checkbox.addEventListener("change", function () {
+    const wrapper = this.closest(".taskWrapper");
+    const id = wrapper.getAttribute("data-id");
+
+    const task = tasksArray.find((taskItem) => taskItem.id == id);
+    task.isDone = this.checked;
+
+    if (task.isDone) {
+      wrapper.querySelector("p").classList.add("doneTask");
+    } else {
+      wrapper.querySelector("p").classList.remove("doneTask");
+    }
+
+  });
+
+  btnDelete.addEventListener("click", function () {
+    const wrapper = this.closest(".taskWrapper");
+    const id = wrapper.getAttribute("data-id");
+    const task = tasksArray.find((taskItem) => taskItem.id == id);
+    const index = tasksArray.indexOf(task);
+    tasksArray.splice(index, 1);
+    text.remove();
+    checkbox.remove();
+    btnDelete.remove();
+    taskWrapper.remove();
+    console.log(tasksArray);
+  });
+  todoForm.elements.task.value = "";
+});
+
+todoForm.querySelectorAll("input").forEach(function (input) {
+  input.addEventListener("input", function () {
+    this.classList.remove(errorClass);
+    this.closest("#todo")
+      .querySelectorAll(".errorText")
+      .forEach((error) => error.remove());
+  });
+});
+
+const filter = document.querySelector(".mySelect");
+filter.addEventListener("change", filterTodos);
+
+function filterTodos(e) {
+  const todos = Array.from(document.querySelectorAll(".taskList", ".taskWrapper"));
+  console.log(todos);
+  todos.forEach(function (todo) {
+    console.log(todo);
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "";
+        break;
+        case "done":
+          if (todo.classList.contains("doneTask")) {
+            todo.style.display = "";
+          } else {
+            todo.style.display = "none";
+          }
+          }
+    });
+  }
+  
+  
+ 
