e76c59ece0e5ff9dd512c7f1db4c78323d3a6475
RuslanSkl <rsklyarenko@gmail.com>
2022-10-15 20:12:58 +0300

    weather widget

--- HW_18/script.js
+++ HW_18/script.js
@@ -6,8 +6,11 @@ let today = new Date().toLocaleDateString();
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-   
-
-
-    
+    document.querySelector(".temperature").innerHTML = data.main.temp + "&degC";
+    document.querySelector(".small").innerHTML = data.weather[0]["description"];
+    document.querySelector(".time").innerHTML = today;
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".feel").innerHTML = data.main.feels_like + "&degC";
+    image.src = `https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@4x.png`;
   });
