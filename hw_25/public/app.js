b8a8a20838949bae09cca5f9a9774508a3dd50f6
RuslanSkl <rsklyarenko@gmail.com>
2023-01-02 16:52:45 +0200

    node server

--- hw_25/public/app.js
+++ hw_25/public/app.js
@@ -1,61 +1,64 @@
-const API_URL = "http://127.0.0.1:3000";
-
-const usersBlock = document.querySelector(".usersBlock");
-
-const addUsersHtml = ({ id, name, age }) => {
-  const div = document.createElement("div");
-  div.setAttribute("data-id", id);
-
-  div.insertAdjacentHTML(
-    "beforeend",
-    `<p><span>User: ${name}; Age: ${age}</span><button data-action="delete">Delete</button></p>`
-  );
-
-  usersBlock.append(div);
-};
-
-const getUsers = () =>
-  fetch(`${API_URL}/users`)
-    .then((response) => {
-      console.log(response);
-      return response.json();
-    })
-    .then((data) => {
-      usersBlock.innerHTML = "";
-      data.forEach(addUsersHtml);
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
+const API_URL = 'http://127.0.0.1:3000';
+const userBlock = document.querySelector('.usersBlock');
+const form = document.forms.user;
+const submitButton = document.getElementById('submit');
 
 getUsers();
 
-document.forms.user.addEventListener("submit", function (event) {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  const name = this.name.value;
+  const name = this.name.value.trim();
   const age = this.age.value;
+  const id = this.id.value;
 
-  fetch(`${API_URL}/users`, {
-    method: "POST",
-    body: JSON.stringify({ name, age }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => addUsersHtml(data))
-    .catch((error) => alert(error.message));
+  if (name == false || age == false) {
+    alert('Can"t submit empty fields');
+    return;
+  }
+
+  if (id) {
+    updateUser(API_URL, name, age, id);
+    const userDom = document.querySelector(`[data-id="${id}"]`);
+    userDom
+      .querySelectorAll('[data-action]')
+      .forEach((item) => (item.disabled = false));
+    submitButton.textContent = 'Add';
+  } else {
+    postUser(API_URL, name, age);
+  }
+
+  form.reset();
 });
 
-usersBlock.addEventListener("click", function (event) {
-  if (event.target.dataset.action !== "delete") return;
-  const id = event.target.closest("[data-id]").dataset.id;
+userBlock.addEventListener('click', function (e) {
+  if (e.target.getAttribute('data-action') === 'delete') {
+    const id = e.target.closest('[data-id]').getAttribute('data-id');
 
-  fetch(`${API_URL}/users/${id}`, {
-    method: "DELETE",
-  })
-    .then((response) => response.json())
-    .then(() => getUsers())
-    .catch((error) => alert(error.message));
+    fetch(`${API_URL}/users/${id}`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then(() => getUsers())
+      .catch((error) => console.log(error.message));
+  }
+
+  if (e.target.getAttribute('data-action') === 'edit') {
+    form.id.value = e.target.closest('[data-id]').getAttribute('data-id');
+
+    form.name.value = e.target
+      .closest('[data-id]')
+      .querySelector('.user-name').textContent;
+
+    form.age.value = e.target
+      .closest('[data-id]')
+      .querySelector('.user-age').textContent;
+
+    e.target.disabled = true;
+    e.target
+      .closest('[data-id]')
+      .querySelector('[data-action="delete"]').disabled = true;
+
+    submitButton.textContent = 'Save';
+  }
 });
--- hw_25/server.js
+++ hw_25/server.js
@@ -1,53 +1,67 @@
-const express = require("express");
-const cors = require("cors");
-const fs = require("fs");
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
+const fs = require('fs');
+const { json } = require('express');
 
 app.use(cors());
-
 app.use(express.json());
-
-app.get("/users", (request, response) => {
-  const users = fs.readFileSync("users.json");
-  return response.send(users);
+app.listen(3000, () => {
+  console.log('Server started at port :3000');
 });
 
-app.post("/users", (req, res) => {
-  // SERVER SIDE VALIDATION
-  const users = fs.readFileSync("users.json");
+
+app.get('/users', (req, res) => {
+  const users = fs.readFileSync('users.json');
+  return res.send(users);
+});
+
+app.delete('/users/:id', (req, res) => {
+  const id = req.params['id'];
+
+  const users = fs.readFileSync('users.json');
   const data = JSON.parse(users);
 
+  const index = data.findIndex((item) => item.id == id);
+
+  if (index !== -1) {
+    data.splice(index, 1);
+    fs.writeFileSync('users.json', JSON.stringify(data));
+    return res.send({ message: 'User has deleted' });
+  } else {
+    return res.status(400).send(`user id:${id} not found`);
+  }
+});
+
+app.post('/users', (req, res) => {
+  const users = fs.readFileSync('users.json');
+  const data = JSON.parse(users);
   const id = data.length === 0 ? 1 : data.at(-1).id + 1;
   const newUser = {
     id,
     ...req.body,
   };
-
   data.push(newUser);
-
-  fs.writeFileSync("users.json", JSON.stringify(data));
-
-  return res.send(JSON.stringify(newUser));
+  fs.writeFileSync('users.json', JSON.stringify(data));
+  return res.send(newUser);
 });
 
-app.delete("/users/:id", (req, res) => {
-  const id = req.params["id"];
+app.put('/users/:id', (req, res) => {
+  const id = req.params['id'];
+  const name = req.body.name;
+  const age = req.body.age;
 
-  const users = fs.readFileSync("users.json");
+  const users = fs.readFileSync('users.json');
   const data = JSON.parse(users);
 
-  const index = data.findIndex((user) => user.id == id);
+  const index = data.findIndex((item) => item.id == id);
 
   if (index !== -1) {
-    data.splice(index, 1);
-    fs.writeFileSync("users.json", JSON.stringify(data));
-    return res.send({ message: "User was deleted." });
+    data[index].name = name;
+    data[index].age = age;
+    fs.writeFileSync('users.json', JSON.stringify(data));
+    return res.send({ message: `user ${id} has updated` });
   } else {
-    return res.status(400).send(`Not found user with id ${id}`);
+    return res.status(400).send(`user id:${id} not found`);
   }
-});
-
-app.listen(3000, () => {
-  console.log("Server started at port :3000");
 });
